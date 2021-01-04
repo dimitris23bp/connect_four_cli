da0ca4f35b3c39a99db6bd9062509587d062e046
grode <dimitris23bp@gmail.com>
2021-01-04 12:59:34 +0200

    Functional game until the first win

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ use std::io::{stdin, stdout, Write};
 use termion::event::Key;
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
-use crate::display::show_game;
 
 struct Settings {
     rows: u8,
@@ -14,7 +13,7 @@ struct Settings {
 fn read_int() -> u8{
     loop {
         let mut input_text = String::new();
-        io::stdin()
+        stdin()
             .read_line(&mut input_text)
             .expect("failed to read from stdin");
 
@@ -51,7 +50,7 @@ fn menu() -> Settings {
 
 mod display {
 
-    pub fn show_game(rows: &u8, cols: &u8, data: &Vec<char>, position: &u8) {
+    pub fn show_game(rows: &u8, cols: &u8, data: &Vec<Vec<char>>, position: &u8) {
         cursor_as_line(cols, position);
         board(rows, cols, data);
     }
@@ -71,19 +70,90 @@ mod display {
     }
 
     // The board of the game
-    pub fn board(rows: &u8, cols: &u8, data: &Vec<char>) {
+    pub fn board(rows: &u8, cols: &u8, data: &Vec<Vec<char>>) {
 
         for row in 0..*rows {
 
             for col in 0..*cols {
                 print!("|");
-                print!("{}", data.get((row * cols + col) as usize).unwrap());
+               // print!("{}", data.get((row * cols + col) as usize).unwrap());
+                print!("{}", data[row as usize][col as usize]);
+            
             }
             print!("|\n\r");
         }
     }
 
+}
 
+fn is_winner(chips: &Vec<Vec<char>>) -> bool {
+
+    for i in 0..chips.len(){
+        let row = chips.get(i).unwrap();
+        for j in 0..row.len() {
+            let chip = row.get(j).unwrap();
+
+            if !chip.eq(&' ') {
+                
+                // Empty values to catch out of index errors
+                let empty_chip: char = ' ';
+                let empty_vec: Vec<char> = Vec::new();
+
+                // If statements are seperated for reading purposes
+
+                // Check vertical
+                if chip.eq(&chips.get(i + 1).unwrap_or(&empty_vec).get(j).unwrap_or(&empty_chip)) 
+                && chip.eq(&chips.get(i + 2).unwrap_or(&empty_vec).get(j).unwrap_or(&empty_chip))
+                && chip.eq(&chips.get(i + 3).unwrap_or(&empty_vec).get(j).unwrap_or(&empty_chip)) {
+                    return true
+                }    
+    
+                // Check horizontal
+                if chip.eq(&chips.get(i).unwrap_or(&empty_vec).get(j + 1).unwrap_or(&empty_chip)) 
+                && chip.eq(&chips.get(i).unwrap_or(&empty_vec).get(j + 2).unwrap_or(&empty_chip))
+                && chip.eq(&chips.get(i).unwrap_or(&empty_vec).get(j + 3).unwrap_or(&empty_chip)) {
+                    return true
+                }    
+    
+                //Check diagonal
+                if chip.eq(&chips.get(i + 1).unwrap_or(&empty_vec).get(j + 1).unwrap_or(&empty_chip)) 
+                && chip.eq(&chips.get(i + 2).unwrap_or(&empty_vec).get(j + 2).unwrap_or(&empty_chip))
+                && chip.eq(&chips.get(i + 3).unwrap_or(&empty_vec).get(j + 3).unwrap_or(&empty_chip)) {
+                    return true
+                }  
+
+                // Check reversed diagonal
+                if chip.eq(&chips.get(i - 1).unwrap_or(&empty_vec).get(j + 1).unwrap_or(&empty_chip)) 
+                && chip.eq(&chips.get(i - 2).unwrap_or(&empty_vec).get(j + 2).unwrap_or(&empty_chip))
+                && chip.eq(&chips.get(i - 3).unwrap_or(&empty_vec).get(j + 3).unwrap_or(&empty_chip)) {
+                    return true
+                }    
+
+            }
+
+    
+
+        }
+
+        
+    }
+
+    false
+
+}
+
+fn load_chip(position: &u8, rows: &u8, chip: &char, data: &mut Vec<Vec<char>>) -> bool{
+
+    let col: usize = (*position).into();
+
+    for i in (0..*rows as usize).rev(){
+        if data[i][col] == ' ' {
+            data[i][col] = *chip;
+            return true
+        }
+    }
+
+    false
 }
 
 fn main() {
@@ -92,15 +162,18 @@ fn main() {
     // Get the basic settings
     let settings = menu();
 
+    // Position of the cursor
     let mut position: u8 = 0;
 
+    let mut chip: char = 'O';
+
     // Initialization of data vector
-    let mut data= vec![' '; (settings.rows * settings.cols) as usize];
+    let mut data = vec![vec![' '; settings.cols.into()]; settings.rows.into()];
 
     let stdin = stdin();
 
     //setting up stdout and going into raw mode
-    let mut stdout = stdout().into_raw_mode().unwrap();
+    let _stdout = stdout().into_raw_mode().unwrap();
 
     print!(
         "{}{}",
@@ -121,7 +194,6 @@ fn main() {
         //     termion::clear::All
         // );
 
-        //i reckon this speaks for itself
         match c.unwrap() {
             Key::Right => {
                 if position < (settings.cols - 1) {
@@ -137,7 +209,33 @@ fn main() {
                 print!("{}{}", termion::cursor::Goto(1, 1), termion::clear::CurrentLine);
                 display::cursor_as_line(&settings.cols, &position);
             },
-            // Key::Down => turn(),
+            Key::Down => {
+
+                // Delete board
+                print!("{}{}", termion::cursor::Goto(1, 2), termion::clear::AfterCursor);
+
+                // Load chip to the data structure
+                let valid_turn: bool = load_chip(&position, &settings.rows, &chip, &mut data);
+                
+                // Display board once again
+                display::board(&settings.rows, &settings.cols, &data);
+                
+                // Check if the current player won
+                let won_game: bool = is_winner(&data);
+                
+                if won_game {
+                    print!("{}{}{}", termion::cursor::Goto(1, 1), termion::clear::All, termion::cursor::Show);
+                    break;
+                }
+                // Change chip, so the other player can play
+                if valid_turn {
+                    if chip == 'O' {
+                        chip = 'X';
+                    } else {
+                        chip = 'O';
+                    }
+                }
+            }
             Key::Ctrl('c') => {
                 print!("{}{}{}", termion::cursor::Goto(1, 1), termion::clear::All, termion::cursor::Show);
                 break;
